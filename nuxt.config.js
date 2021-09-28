a7ef3adc0ac7c9e4ad83ea17dedca93aa4fd8b3d
jonathan_ellos <jonathan.marquez@ellos.se>
2021-09-28 17:08:37 +0200

    agin

--- nuxt.config.js
+++ nuxt.config.js
@@ -78,7 +78,6 @@ export default {
     strategies: {
       google: {
         client_id: "37049723745-3fkv14o86jki23ecrqhtueutqnu8nb9m.apps.googleusercontent.com",
-        responseType: 'token id_token',
         redirect_uri: 'https://silly-khorana-e15180.netlify.app/login/'
       }
     },
